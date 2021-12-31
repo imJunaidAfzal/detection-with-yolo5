2b91b22019856ba119661db84761b4828b8ddc1d
Pakeeza Ehsan <pakeeza.ehsaan@arhamsoft.org>
2021-12-31 18:25:02 +0500

    updated

--- checking_yolo5.py
+++ checking_yolo5.py
@@ -1,7 +1,6 @@
 import glob
 import logging
 import os
-
 import Images_from_Video
 import torch
 
@@ -12,8 +11,10 @@ class DetectObject:
     """
 
     def __init__(self):
+        self.__frames_time = {}
         self.__model = torch.hub.load('ultralytics/yolov5', 'yolov5x', pretrained=True)
         self.__results = None
+        self.response = {}
         """
                 Create and configure logger
                 """
@@ -57,9 +58,10 @@ class DetectObject:
         img = Images_from_Video.ImagesFromVideo(self.logger,
                                                 f_name)
         cam = img.readfile()
+        self.__frames_time = img.get_frames_times(cam)
         img.processing(cam)
-        img.detect_duplicate_images()
-        images = glob.glob(os.path.dirname(os.path.abspath(__file__)) + '/'  + "frames/*.jpg")
+        # img.detect_duplicate_images()
+        images = glob.glob(os.path.dirname(os.path.abspath(__file__)) + '/' + "frames/*.jpg")
         return images
 
     def detect_objects(self, image):
@@ -73,17 +75,36 @@ class DetectObject:
         self.__results = self.__model(image)
         total = 0
         classes_count = {}
+        i = 1
+        time = []
+        frames_name = []
         for frame in self.__results.pandas().xyxy:
             detected_classes = set(frame['name'])
             list_objects = list(frame['name'])
-
+            i += 1
             for obj in detected_classes:
                 if obj in classes_count:
                     classes_count[obj] += list_objects.count(obj)
+
                 else:
+                    self.response[obj] = {}
                     classes_count[obj] = list_objects.count(obj)
+
+                print("hello")
+                initial_time = (i - 1) / self.__frames_time["frames_per_second"]
+                end_time = i / self.__frames_time["frames_per_second"]
+                time.append([initial_time, end_time])
+                frames_name.append(str(i - 1))
+
+                print("world")
+                self.response[obj]["time"] = time
+                self.response[obj]["frames"] = frames_name
+                self.response[obj]["occurrence"] = classes_count[obj]
                 total += list_objects.count(obj)
 
         classes_count['All'] = total
+        print("Response: ")
+        for i in self.response.items():
+            print(i)
 
         return classes_count
